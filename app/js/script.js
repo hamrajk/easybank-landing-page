765a098a799cbe1c5e28a0f0371ef07bcdc0a38b
hamrajk <hamraj_khangura@hotmail.com>
2022-02-11 14:09:35 -0500

    Animated the hamburger menu to become an x when opened, and return to hamburger lines once closed

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("clicked hamburger");
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
