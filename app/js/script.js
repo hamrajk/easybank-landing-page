8d1dba57dc7c105b61e091637f8edd889d59d7cb
hamrajk <hamraj_khangura@hotmail.com>
2022-02-12 16:29:14 -0500

    Added animation for pop up menu on clicking the hamburger icon. Menu fades in and out in sync with the overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,26 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
 
 btnHamburger.addEventListener("click", function () {
   console.log("clicked hamburger");
   if (header.classList.contains("open")) {
     //closing the hamburger menu
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    body.classList.remove("noscroll");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     //opening the hamburger menu
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    body.classList.add("noscroll");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
