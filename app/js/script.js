7172351c5fb45e3c1f597edbdd76732e02b463a8
hamrajk <hamraj_khangura@hotmail.com>
2022-02-11 19:43:12 -0500

    Added animation for pop up menu when hamburger icon is clicked

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,18 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
   console.log("clicked hamburger");
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
+  if (header.classList.contains("open")) {
+    //closing the hamburger menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
   } else {
-    btnHamburger.classList.add("open");
+    //opening the hamburger menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
